99ce026868972d94b6fb3113473330e9e431f291
Nealadam1 <78753918+Nealadam1@users.noreply.github.com>
2022-11-23 21:10:30 +0200

    wednsday 21:00 commit

--- js/game.js
+++ js/game.js
@@ -8,11 +8,12 @@ const FLAG = '<img src="img/flag.png"></img>'
 var gBoard
 var gGame
 var gLevel
+var gFirstClick
 
 
 function initGame() {
     gBoard = []
-    gGame = { isOn: false, shownCount: 0, markedCount: 0, secsPassed: 0 }
+    gGame = { isOn: false, shownCount: 0, markedCount: 0, secsPassed: 0, mineSpotted: 0 }
     document.addEventListener('contextmenu', event => event.preventDefault());
 
 
@@ -20,7 +21,14 @@ function initGame() {
 }
 
 function playGame(size, mines) {
+    gBoard = []
     gGame.isOn = true
+    gGame.markedCount = 0
+    gGame.mineSpotted = 0
+    gGame.shownCount = 0
+    gGame.secsPassed = 0
+    gFirstClick = true
+
     gLevel = {
         size,
         mines
@@ -28,8 +36,10 @@ function playGame(size, mines) {
     console.log('gLevel:', gLevel)
     gBoard = buildBoard(gLevel)
     console.log('gBoard:', gBoard)
+    clearInterval(gGame.timerInterval)
     renderBoard(gBoard)
-    spawnMines(gBoard, mines)
+    renderInfo()
+
 }
 
 function buildBoard(level) {
@@ -41,7 +51,7 @@ function buildBoard(level) {
                 isShown: false,
                 isMine: false,
                 isMarked: false,
-                minesAroundCount:0
+                minesAroundCount: 0
             }
             board[i][j] = cell
         }
@@ -56,14 +66,14 @@ function renderBoard(board) {
         for (var j = 0; j < board[0].length; j++) {
             const currCell = board[i][j]
             var className = ''
-            var strTEXT=''
+            var strTEXT = ''
             //Type of cell
             if (currCell.isShown) {
                 className = 'shown'
                 strTEXT = ''
             }
 
-            if (currCell.isShown&&currCell.minesAroundCount>0) {
+            if (currCell.isShown && currCell.minesAroundCount > 0) {
                 className = 'shown'
                 strTEXT = +currCell.minesAroundCount
             }
@@ -89,17 +99,47 @@ function renderBoard(board) {
 
 function CellClicked(elCell, i, j, ev) {
     var cell = gBoard[i][j]
+    if (!gGame.isOn) return
+
+    if (gFirstClick) {
+        startTimer()
+        cell.isShown = true
+        spawnMines(gBoard, gLevel.mines)
+        gFirstClick = false
+
+    }
     switch (ev.button) {
         case 0:
-            cell.isShown = true
+            if (cell.isMarked) return
+            if (cell.minesAroundCount > 0) {
+                cell.isShown = true
+            } else if (cell.minesAroundCount===0){
+                expandShown(gBoard, i, j)
+            }
+
+            gGame.shownCount++
+
             break
         case 2:
             cell.isMarked = (cell.isMarked) ? false : true
+            if (cell.isMarked) {
+                gGame.markedCount++
+                if (cell.isMine) gGame.mineSpotted++
+            } else {
+                gGame.markedCount--
+                if (cell.isMine)
+                    gGame.mineSpotted--
+            }
+
+            console.log('gGame.markedCount:', gGame.markedCount)
+            console.log('gGame.mineSpotted:', gGame.mineSpotted)
 
             break
     }
     setMinesNegsCount(gBoard)
+    renderInfo()
     renderBoard(gBoard)
+    checkGameOver(elCell, i, j)
 
 
 
@@ -117,7 +157,8 @@ function spawnMines(board, amount) {
     for (var d = 0; d < amount; d++) {
         const posI = minesIdx[d].i
         const posJ = minesIdx[d].j
-        board[posI][posJ].isMine = true
+        if (!board[posI][posJ].isShown)
+            board[posI][posJ].isMine = true
 
     }
 
@@ -129,18 +170,57 @@ function cellMarked(elCell) {
 
 }
 
-function checkGameOver() {
+function checkGameOver(elCell, i, j) {
+    const cell = gBoard[i][j]
+    if (cell.isMine && cell.isShown) gameOver(elCell)
+    if (gGame.mineSpotted === gLevel.mines && gGame.shownCount === gLevel.size ** 2 - gLevel.mines) victory()
+
+
+
+
 
 }
 
-function expandShown(board, elCell, i, j) { }
+function gameOver(elCell) {
+    console.log('gameover')
+    clearInterval(gGame.timerInterval)
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard.length; j++) {
+            if (gBoard[i][j].isMine) gBoard[i][j].isShown = true
+        }
+    }
+    console.log(gBoard)
+    gGame.isOn = false
+    renderBoard(gBoard)
+
+}
+function victory() {
+    console.log('win')
+}
+
+function expandShown(board, i, j) {
+    if(i<0 || i> board.length-1||j<0||j>board.length-1) return
+    console.log('expand')
+    console.log(i,j)
+    console.log('board[i][j].isShown:', board[i][j].isShown)
+    console.log('board[i][j].minesAroundCount:', board[i][j].minesAroundCount)
+    if (board[i][j].minesAroundCount=== 0 && !board[i][j].isShown) {
+        console.log('exppand loop')
+        board[i][j].isShown = true;
+        expandShown(board, i + 1, j)
+        expandShown(board, i - 1, j)
+        expandShown(board, i, j + 1)
+        expandShown(board, i, j - 1)
+    } else {
+        return;
+    }
+}
 
 function setMinesNegsCount(board) {
-    
     for (var i = 0; i < board.length; i++) {
         for (var j = 0; j < board.length; j++) {
             const cell = board[i][j]
-            cell.minesAroundCount = countNegs({ i: i, j: j })
+            cell.minesAroundCount = +countNegs({ i: i, j: j })
         }
     }
 
@@ -159,4 +239,42 @@ function countNegs(pos) {
         }
     }
     return negMines
+}
+function startTimer() {
+    const startTime = Date.now()
+    gGame.timerInterval = setInterval(
+        () => updateTime(startTime),
+        1000
+    )
+}
+
+function updateTime(startTime) {
+    const currTime = Date.now()
+    const elapsed = currTime - startTime
+    const secsElapsed = elapsed / 1000
+    console.log('elapsed:', secsElapsed)
+
+    gGame.secsPassed = parseInt(secsElapsed)
+
+    const elTimer = document.querySelector('.timer')
+
+    var zeroFix = '00'
+    if (gGame.secsPassed >= 10) zeroFix = '0'
+    if (gGame.secsPassed >= 100) zeroFix = ''
+    elTimer.innerText = `${zeroFix}${gGame.secsPassed}`
+
+    // if (gGame.secsPassed <= gMaxCount) renderTime(gGame.secsPassed)
+    //still count towards best time but not update past MAX_TIME
+}
+
+
+function renderInfo(){
+    renderFlagCount()
+    
+}
+
+function renderFlagCount(){
+    const elFlagCount=document.querySelector('.flag-count')
+    elFlagCount.innerText
+
 }
